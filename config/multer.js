6f0221e5bce568ae69eb5fc8ad38c60ead40194b
Anderson <philipe0611@gmail.com>
2019-04-23 10:20:42 -0300

    teste

--- config/multer.js
+++ config/multer.js
@@ -3,11 +3,11 @@ const path = require('path');
 const crypto = require('crypto');
 
 module.exports = {
-    dest: path.resolve(__dirname, '.', 'tmp'),
+    dest: path.resolve(__dirname, '..', 'tmp'),
     storage: multer.diskStorage({
         
         destination: (req, file, cb) => {
-            cb(null, path.resolve(__dirname, '.', 'tmp'));
+            cb(null, path.resolve(__dirname, '..', 'tmp'));
         },
 
         filename: (req, file, cb) => {
