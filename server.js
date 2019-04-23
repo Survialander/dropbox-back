c63f193a078da72677e91a946af4a0cad8404d9d
Anderson <philipe0611@gmail.com>
2019-04-23 14:34:53 -0300

    teste

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/arquivos', express.static(path.resolve(__dirname, '.', 'tmp')));
+app.get('/arquivos', express.static(path.resolve(__dirname, '.', 'tmp')));
 app.use(require('./routes'));    
 
 app.listen(process.env.PORT || 3333, () => {
